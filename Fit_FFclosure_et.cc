76727b3219be32e32f12b883ddee3fb95af77d30
ccaillol <ccaillol@cern.ch>
2019-09-23 06:18:07 -0400

    add fit uncertainties

--- Fit_FFclosure_et.cc
+++ Fit_FFclosure_et.cc
@@ -69,7 +69,7 @@ Double_t fitFunc_Exp3Par(Double_t *x, Double_t *par) {
 
 Double_t fitFunc_Line2Par(Double_t *x, Double_t *par) {
     //return par[0] + par[1] * x[0] + par[2] * x[0]* x[0];// + par[3] * x[0]* x[0] *x[0];
-    return par[0] + par[1]* (x[0]-80);
+    return par[0] + par[1]* (x[0]-150);
     //    return par[0] + par[1]*x[0] + par[2]*(TMath::Landau(x[0],par[3],par[4],0));
 //return par[0] + par[1]*(TMath::Exp(par[2] * x[0]-par[3]));
 }
@@ -122,23 +122,88 @@ TF1 *M_FR(int WP, std::string type, std::string files, std::string num, std::str
     t.SetTextAlign(12);
     t.SetTextSize(0.04);
     t.DrawLatex(0.55, .96, "59.5 fb^{-1} (2018, 13 TeV)");
-    float aup; float adown; float bup; float bdown;
     theFit->Draw("SAME");
     theFit->SetLineColor(2);
 
+    // Up and down fits
+    Double_t TauLegParameters[2];
+    theFit->GetParameters(TauLegParameters);
+
+    Double_t matrix[2][2];
+    gMinuit->mnemat(&matrix[0][0],2);
+    TMatrixD mat_D(2,2);
+     for (int i=0; i<2; ++i){
+        for (int j=0; j<2; ++j){
+             mat_D[i][j]=matrix[i][j];
+        }
+    }
+    float aup; float adown; float bup; float bdown;
+
+    TMatrixDEigen mat_sym=TMatrixDEigen (mat_D);
+    TMatrixD eigenValues=mat_sym.GetEigenValues();
+    TMatrixD eigenVectors=mat_sym.GetEigenVectors();
+    TMatrixD eigenVectorsInverted=mat_sym.GetEigenVectors();
+    aup = TauLegParameters[0]+eigenVectorsInverted[0][0]*sqrt(eigenValues[0][0])+eigenVectorsInverted[0][1]*sqrt(eigenValues[1][1]);
+    bup = TauLegParameters[1]+eigenVectorsInverted[0][1]*sqrt(eigenValues[0][0])+eigenVectorsInverted[1][1]*sqrt(eigenValues[1][1]);
+    adown = TauLegParameters[0]-eigenVectorsInverted[0][0]*sqrt(eigenValues[0][0])-eigenVectorsInverted[0][1]*sqrt(eigenValues[1][1]);
+    bdown = TauLegParameters[1]-eigenVectorsInverted[0][1]*sqrt(eigenValues[0][0])-eigenVectorsInverted[1][1]*sqrt(eigenValues[1][1]);
+
+    float au1=TauLegParameters[0]+eigenVectorsInverted[0][0]*sqrt(eigenValues[0][0]);
+    float bu1=TauLegParameters[1]+eigenVectorsInverted[0][1]*sqrt(eigenValues[0][0]);
+    float ad1=TauLegParameters[0]-eigenVectorsInverted[0][0]*sqrt(eigenValues[0][0]);
+    float bd1=TauLegParameters[1]-eigenVectorsInverted[0][1]*sqrt(eigenValues[0][0]);
+    float au2=TauLegParameters[0]+eigenVectorsInverted[0][1]*sqrt(eigenValues[1][1]);
+    float bu2=TauLegParameters[1]+eigenVectorsInverted[1][1]*sqrt(eigenValues[1][1]);
+    float ad2=TauLegParameters[0]-eigenVectorsInverted[0][1]*sqrt(eigenValues[1][1]);
+    float bd2=TauLegParameters[1]-eigenVectorsInverted[1][1]*sqrt(eigenValues[1][1]);
+
+    TF1 * theFitup1 = new TF1("theFit", fitFunc_Line2Par, fMin, fMax, 2);
+    theFitup1->SetParameter(0, au1);
+    theFitup1->SetParameter(1, bu1);
+    theFitup1->SetLineColor(kViolet+1);
+    theFitup1->Draw("same");
+    TF1 * theFitup2 = new TF1("theFit", fitFunc_Line2Par, fMin, fMax, 2);
+    theFitup2->SetParameter(0, au2);
+    theFitup2->SetParameter(1, bu2);
+    theFitup2->SetLineColor(kGreen-3);
+    theFitup2->Draw("same");
+    TF1 * theFitdown1 = new TF1("theFit", fitFunc_Line2Par, fMin, fMax, 2);
+    theFitdown1->SetParameter(0, ad1);
+    theFitdown1->SetParameter(1, bd1);
+    theFitdown1->SetLineColor(kViolet+1);
+    theFitdown1->Draw("same");
+    TF1 * theFitdown2 = new TF1("theFit", fitFunc_Line2Par, fMin, fMax, 2);
+    theFitdown2->SetParameter(0, ad2);
+    theFitdown2->SetParameter(1, bd2);
+    theFitdown2->SetLineColor(kGreen-3);
+    theFitdown2->Draw("same");
+    TLegend *l = new TLegend(0.15, 0.74, 0.5, 0.89, NULL, "brNDC");
+    l->SetBorderSize(0);
+    l->SetFillColor(0);
+    l->SetTextSize(.03);
+    l->SetFillColor(0);
+
+    l->SetTextSize(.03);
+    l->SetFillColor(0);
+    l->AddEntry(theFit, "Best fit", "l");
+    l->AddEntry(theFitup1, "1st uncertainty #pm 1#sigma", "l");
+    l->AddEntry(theFitup2, "2nd uncertainty #pm 1#sigma", "l");
+    l->Draw("same");
+
     canvas->SaveAs(outNaming.c_str());
 
-    if (denum.find("Eta") < 140 or denum.find("Pt") < 140){
-       hist2D_lep->SetBinContent(WP, 5, aup);
-       hist2D_lep->SetBinContent(WP, 7, bup);
-       hist2D_lep->SetBinContent(WP, 9, adown);
-       hist2D_lep->SetBinContent(WP, 11, bdown);
-    }
-
-    TFile *FR_H = new TFile("FitHistograms_FR.root", "UPDATE");
+    TFile *FR_H = new TFile("FF_corrections_1.root", "UPDATE");
     FR_H->cd();
-    TGraph_FR->SetName(TString(num) + "_" + TString(denum));
-    TGraph_FR->Write();
+    theFit->SetName(TString(name));
+    theFit->Write();
+    theFitup1->SetName(TString(name)+"_unc1_up");
+    theFitup1->Write();
+    theFitdown1->SetName(TString(name)+"_unc1_down");
+    theFitdown1->Write();
+    theFitup2->SetName(TString(name)+"_unc2_up");
+    theFitup2->Write();
+    theFitdown2->SetName(TString(name)+"_unc2_down");
+    theFitdown2->Write();
     FR_H->Close();
 
     return theFit;
@@ -148,29 +213,17 @@ void Fit_FFclosure_et() {
 
     gStyle->SetOptFit(1111);
 
-    TFile *FR_File = new TFile("FF_corrections_1.root", "RECREATE");
-
     TH2F * Fit_Value_tau = new TH2F("Fit_Value_tau", "Fit_Value_tau", 40, 0, 40, 40, 0, 40);
 
     Double_t fMin = 0;
     Double_t fMax = 300;
 
     TF1* m11 = M_FR(1, "Line2Par", "files_corr1FF_et/DataSub.root", "et_0jet_qcd_iso", "et_0jet_qcd_anti", "closure_mvis_et_qcd", Fit_Value_tau, fMin, fMax);
-    TF1* m12 = M_FR(2, "Line2Par", "files_corr1FF_et/DataSub.root", "et_0SSloose_qcd_iso", "et_0SSloose_qcd_anti", "closure_mvis_et_qcdSSloose", Fit_Value_tau, fMin, fMax);
+    TF1* m12 = M_FR(2, "Line2Par", "files_corr1FF_et/DataSub.root", "et_0SSloose_qcd_iso", "et_0SSloose_qcd_anti", "closure_mvis_et_qcdloose", Fit_Value_tau, fMin, fMax);
     TF1* m13 = M_FR(3, "Line2Par", "files_corr1FF_et/DataSub.root", "et_0jet_w_iso", "et_0jet_w_anti", "closure_mvis_et_w", Fit_Value_tau, fMin, fMax);
     TF1* m15 = M_FR(5, "Line2Par", "files_corr1FF_et/DataSub.root", "et_0jet_tt_iso", "et_0jet_tt_anti", "closure_mvis_et_tt", Fit_Value_tau, fMin, fMax);
     TF1* m17 = M_FR(7, "Line2Par", "files_corr1FF_et/WMC.root", "et_0jet_w_iso/WMC", "et_0jet_w_anti/WMC", "closure_mvis_et_wmc", Fit_Value_tau, fMin, fMax);
     TF1* m18 = M_FR(8, "Line2Par", "files_corr1FF_et/TTMC.root", "et_0jet_tt_iso/TTMCJ", "et_0jet_tt_anti/TTMCJ", "closure_mvis_et_ttmc", Fit_Value_tau, fMin, fMax);
 
-    
-    FR_File->Write();
-    FR_File->cd();
-    m11->SetName("closure_mvis_et_qcd"); m11->Write();
-    m12->SetName("closure_mvis_et_qcdloose"); m12->Write();
-    m13->SetName("closure_mvis_et_w"); m13->Write();
-    m15->SetName("closure_mvis_et_tt"); m15->Write();
-    m17->SetName("closure_mvis_et_wmc"); m17->Write();
-    m18->SetName("closure_mvis_et_ttmc"); m18->Write();
-    FR_File->Close();
 }
 
